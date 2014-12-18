4afb9f44b6271e156a731990f2b0d04098c1b072
Gustavo Rodriguez <gustavo.rodriguez@globallogic.com>
2014-12-18 17:19:40 -0300

    Commit inicial. Carga de la primera escena y dibujo en el canvas. Se trabajo con un tileset con mosaicos de 32x32 pixeles, en un canvas de 640 x 480. Se crearon dos layers: foreground,  que representa los elementos colisionables, y backgound que representa el fondo inactivo. Se consume la matriz del mapa desde un ApiController que sirve Json.

---
+++ MisteryForest/Api/MapController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MisteryForest.Api
+{
+    using System.IO;
+    using System.Net.Http.Headers;
+    using System.Text;
+
+    public class MapController : ApiController
+    {
+        // GET api/map
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/map/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/map
+        public HttpResponseMessage Post([FromBody]string value)
+        {
+            var path = @"C:\Projects\MisteryForest\MisteryForest\MisteryForest\maps\level1-tilemap.json";
+
+            var sr = new StreamReader(path);
+
+            var jsonString = sr.ReadToEnd();
+
+            var response = Request.CreateResponse(HttpStatusCode.OK, jsonString);
+
+            response.Content = new StringContent(jsonString, Encoding.UTF8, "application/json");
+
+            sr.Close();
+ 
+            return response;
+        }
+
+        // PUT api/map/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/map/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
